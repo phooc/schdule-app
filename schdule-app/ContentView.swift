a00caee4f030c229543119be285e9a24e37e1c79
phooc <bjoe7382@gmail.com>
2024-06-03 19:35:03 -0500

    Initial Commit

---
+++ schdule-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  schdule-app
+//
+//  Created by Phuc Tran on 6/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ schdule-app/schdule_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  schdule_appApp.swift
+//  schdule-app
+//
+//  Created by Phuc Tran on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct schdule_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
