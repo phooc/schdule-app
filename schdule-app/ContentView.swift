94433ae6306a5e89563a46eb20b87386485285ca
phooc <bjoe7382@gmail.com>
2024-06-03 19:42:53 -0500

    this was a test

--- schdule-app/ContentView.swift
+++ schdule-app/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "globex")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("leo stinks!")
         }
         .padding()
     }
